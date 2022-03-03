be06542b5016720bc3e1e2054e5de5d9414cb66e
jagamiz <jagamiz@iestrassierra.com>
2022-03-03 11:17:55 +0100

    Primer commit AppEjemplo2

---
+++ src/appejemplo2/Clase1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package appejemplo2;
+
+/**
+ *
+ * @author igo 2021
+ */
+public class Clase1 {
+
+}
